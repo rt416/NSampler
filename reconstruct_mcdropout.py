11740e4ce177f4c60aa42ef49c9ed662bbba2e62
Ryutaro Tanno <Ryutaro.Tanno.15@ucl.ac.uk>
2017-01-23 20:08:45 +0000

    try again

--- reconstruct_mcdropout.py
+++ reconstruct_mcdropout.py
@@ -89,7 +89,7 @@ def mc_inference(fn, fn_std, fd, opt):
             sum_out2 += 1.*fn.eval(feed_dict=fd)**2
 
         mean = sum_out / (1.*no_samples)
-        std = np.sqrt((sum_out2 - 2*mean*sum_out + no_samples*mean**2)/no_samples)
+        std = np.sqrt(np.abs(sum_out2 - 2*mean*sum_out + no_samples*mean**2)/no_samples)
         print(std)
     elif opt['method']=='cnn_heteroscedastic':
         sum_out = 0.0
@@ -99,7 +99,7 @@ def mc_inference(fn, fn_std, fd, opt):
             sum_out2 += 1. * fn.eval(feed_dict=fd) ** 2
 
         mean = sum_out / no_samples
-        std = np.sqrt((sum_out2 - 2 * mean * sum_out + no_samples * mean ** 2) / no_samples)
+        std = np.sqrt(np.abs(sum_out2 - 2 * mean * sum_out + no_samples * mean ** 2) / no_samples)
         std += 1. * fn_std.eval(feed_dict=fd)
     else:
         raise Exception('The specified method does not support MC inference.')
