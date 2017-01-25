fixed a mistake in the kl divergence

models.py
@@ -280,8 +280,8 @@ def inference(method, x, y, keep_prob, opt):
 
     elif method == 'cnn_variational_dropout' or \
          method == 'cnn_variational_dropout_layerwise' or \
-         method == 'cnn_variational_dropout_average' or \
-         method == 'cnn_variational_dropout_channelwise':
+         method == 'cnn_variational_dropout_channelwise' or \
+         method == 'cnn_variational_dropout_average':
 
         if method == 'cnn_variational_dropout':
             params='weight'
@@ -327,10 +327,11 @@ def inference(method, x, y, keep_prob, opt):
             tf.summary.scalar('kl_div_average', kl_div)
 
         with tf.name_scope('expected_loglikelihood'):
-            e_loglike = tf.reduce_mean(tf.square(y - y_pred))
+            e_loglike = tf.reduce_mean(tf.reduce_sum(tf.square(y - y_pred),[1,2,3,4]),0)
             if not(method == 'cnn_variational_dropout_average'):
                 e_loglike = opt['train_noexamples'] * e_loglike
             tf.summary.scalar('e_loglike', e_loglike)
+
         with tf.name_scope('loss'):
             cost = tf.add(e_loglike, kl_div, name='ELBO')
             tf.summary.scalar('average_ELBO', cost)
@@ -412,15 +413,12 @@ def inference(method, x, y, keep_prob, opt):
             y_std = tf.sqrt(1. / y_prec, name='y_std')
 
         with tf.name_scope('expected_loglikelihood'):
-            e_loglike = tf.reduce_mean(tf.square(tf.mul(y_prec, (y - y_pred)))) \
-                        - tf.reduce_mean(tf.log(y_prec))
-
-            # e_loglike = tf.reduce_mean(tf.reduce_sum(tf.square(tf.mul(y_prec, (y - y_pred))), [1,2,3,4]), 0) \
-            #           - tf.reduce_mean(tf.reduce_sum(tf.log(y_prec), [1,2,3,4]), 0)
-
-            if not (method == 'cnn_variational_dropout_average'):
+            e_loglike = tf.reduce_mean(tf.reduce_sum(tf.square(tf.mul(y_prec, (y - y_pred))), [1,2,3,4]), 0) \
+                      - tf.reduce_mean(tf.reduce_sum(tf.log(y_prec), [1,2,3,4]), 0)
+            if not (method == 'cnn_heteroscedastic_variational_average'):
                 e_loglike = opt['train_noexamples'] * e_loglike
             tf.summary.scalar('e_loglike', e_loglike)
+
         with tf.name_scope('loss'):
             cost = tf.add(e_loglike, kl_div, name='ELBO')
             tf.summary.scalar('cost', cost)