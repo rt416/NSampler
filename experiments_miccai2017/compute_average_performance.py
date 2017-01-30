1c0d67b1ee117b9b49a3ccdf3d68b59e424db079
Ryutaro Tanno <Ryutaro.Tanno.15@ucl.ac.uk>
2017-01-30 20:34:50 +0000

    perform a large scale comparative analysis

--- experiments_miccai2017/compute_average_performance.py
+++ experiments_miccai2017/compute_average_performance.py
@@ -1,14 +1,14 @@
-"""Ryu: main experiments script"""
-
-import numpy as np
-import matplotlib.pyplot as plt
+""" Main script for running statistical analysis for MICCAI 2017 """
 import tensorflow as tf
+import numpy as np
+import cPickle as pkl
+import os
 
-# Options
+# Default options
 opt = {}
 
 # Network:
-opt['method'] = 'cnn_simple'
+opt['method'] = 'cnn_heteroscedastic'
 opt['n_h1'] = 50
 opt['n_h2'] = 2*opt['n_h1']
 opt['n_h3'] = 10
@@ -28,12 +28,13 @@ opt['validation_fraction'] = 0.5
 # Data/task:
 opt['cohort'] ='Diverse'
 opt['no_subjects'] = 8
+opt['patchlib_idx'] = 1
 opt['b_value'] = 1000
 opt['no_randomisation'] = 1
 opt['shuffle_data'] = True
 opt['chunks'] = True  # set True if you want to chunk the HDF5 file.
 
-opt['subsampling_rate'] = 85
+opt['subsampling_rate'] = 343
 opt['upsampling_rate'] = 2
 opt['input_radius'] = 5
 opt['receptive_field_radius'] = 2
@@ -44,9 +45,9 @@ opt['transform_opt'] = 'standard'  # preprocessing of input/output variables
 
 # Dir:
 opt['data_dir'] = '/SAN/vision/hcp/Ryu/IPMI2016/TrainingSet/' # '../data/'
-opt['save_dir'] = '/SAN/vision/hcp/Ryu/miccai2017/models'
-opt['log_dir'] = '/SAN/vision/hcp/Ryu/miccai2017/log'
-opt['recon_dir'] = '/SAN/vision/hcp/Ryu/miccai2017/recon'
+opt['save_dir'] = '/SAN/vision/hcp/Ryu/miccai2017/comparison/models'
+opt['log_dir'] = '/SAN/vision/hcp/Ryu/miccai2017/comparison/log'
+opt['recon_dir'] = '/SAN/vision/hcp/Ryu/miccai2017/comparison/recon'
 
 opt['save_train_dir_tmp'] = '/SAN/vision/hcp/Ryu/IPMI2016/HCP'
 opt['save_train_dir'] = '/SAN/vision/hcp/Ryu/IPMI2016/TrainingSet/'
@@ -54,74 +55,102 @@ opt['save_train_dir'] = '/SAN/vision/hcp/Ryu/IPMI2016/TrainingSet/'
 opt['gt_dir'] = '/SAN/vision/hcp/DCA_HCP.2013.3_Proc/'  # ground truth dir
 opt['subpath'] = 'T1w/Diffusion'
 
+opt['mask_dir'] ='/Users/ryutarotanno/tmp/recon'
+
+
 opt['input_file_name'] = 'dt_b1000_lowres_' + str(opt['upsampling_rate']) + '_'
 
-# Train standard networks on training sets of varying size:
-from train import train_cnn
-import reconstruct
 
-# opt['method'] = 'cnn_simple'
-#
-# for subsample in [5500, 1372, 343, 85]:
-#     # Train:
-#     opt['subsampling_rate'] = subsample
-#     train_cnn(opt)
-#
-#     # Reconstruct (optional):
-#     subjects_list = ['904044', '165840', '889579', '713239',
-#                      '899885', '117324', '214423', '857263']
-#     rmse_average = 0
-#     for subject in subjects_list:
-#         opt['subject'] = subject
-#         rmse, _ = reconstruct.sr_reconstruct(opt)
-#         rmse_average += rmse
-#
-#     print('\n Average RMSE on Diverse dataset is %.15f.'
-#               % (rmse_average / len(subjects_list),))
+# Define model updates:
+def models_update(idx, opt):
+    if idx == 1:
+        opt['method'] = 'cnn_simple'
+        opt['dropout_rate'] = 0.0
+        name = opt['method']
+    elif idx == 2:
+        opt['method'] = 'cnn_dropout'
+        opt['dropout_rate'] = 0.1
+        name = opt['method'] + '_0.1'
+    elif idx == 3:
+        opt['method'] = 'cnn_gaussian_dropout'
+        opt['dropout_rate'] = 0.1
+        name = opt['method'] + '_0.1'
+    elif idx == 4:
+        opt['method'] = 'cnn_variational_dropout'
+        opt['dropout_rate'] = 0.0
+        name = opt['method']
+    elif idx == 5:
+        opt['method']='cnn_variational_dropout_channelwise'
+        opt['dropout_rate'] = 0.0
+        name = opt['method']
+    elif idx == 6:
+        opt['method'] == 'cnn_heteroscedastic'
+        opt['dropout_rate'] = 0.0
+        name = opt['method']
+    elif idx == 7:
+        opt['method'] == 'cnn_heteroscedastic_downsc'
+        opt['dropout_rate'] = 0.0
+        name = opt['method']
+    elif idx == 8:
+        opt['method'] == 'cnn_heteroscedastic_channelwise'
+        opt['dropout_rate'] = 0.0
+        name = opt['method']
+    else:
+        raise ValueError('no network for the given idx.')
 
-
-# Train dropout network
-print('Train dropout networks!')
-opt['method'] = 'cnn_dropout'
-opt['subsampling_rate'] = 686
-
-for dropout_rate in [0.0, 0.1, 0.2, 0.3, 0.5]:
-    # Train:
-    opt['dropout_rate'] = dropout_rate
-    train_cnn(opt)
-
-    # Reconstruct (optional):
-    subjects_list = ['904044', '165840', '889579', '713239',
-                     '899885', '117324', '214423', '857263']
-    rmse_average = 0
-    for subject in subjects_list:
-        opt['subject'] = subject
-        rmse, _ = reconstruct.sr_reconstruct(opt)
-        rmse_average += rmse
-
-    print('\n Average RMSE on Diverse dataset is %.15f.'
-          % (rmse_average / len(subjects_list),))
+    return name, opt
 
 
-# MC dropout reconstruct:
-import reconstruct_mcdropout
-print('Train dropout networks!')
-opt['method'] = 'cnn_dropout'
-opt['subsampling_rate'] = 686
-opt['mc_no_samples'] = 100
+# Start the experiment:
+import analysis_miccai2017
+
+analysis_dir = '/SAN/vision/hcp/Ryu/miccai2017/comparison/analysis'
+experiment_name = '30jan17_compare_models'
+subjects_list = ['904044', '165840', '889579', '713239',
+                 '899885', '117324', '214423', '857263']
+err_compare = dict()
+
+for model_idx in range(1,9):
+    err_mtx = np.zeros((len(subjects_list),8,6))
+    name, opt=models_update(model_idx,opt)
+    print("Compute average errors ...")
+    for i,patch_idx in enumerate(range(1,9)):
+        opt['patchlib_idx'] = patch_idx
+        for j, subject in enumerate(subjects_list):
+            opt['subject'] = subject
+            err = analysis_miccai2017.compute_err(opt)
+            err_mtx[j,i,0] = err['rmse_noedge']
+            err_mtx[j,i,1] = err['psnr_noedge']
+            err_mtx[j,i,2] = err['mssim_noedge']
+            err_mtx[j,i,3] = err['rmse_whole']
+            err_mtx[j,i,4] = err['psnr_whole']
+            err_mtx[j,i,5]= err['mssim_whole']
+
+    err_compare[name]\
+    = {'mean':
+           {'rmse_noedge':err_mtx.mean(axis=(0,1))[0],
+            'psnr_noedge':err_mtx.mean(axis=(0,1))[1],
+            'mssim_noedge':err_mtx.mean(axis=(0,1))[2],
+            'rmse_whole':err_mtx.mean(axis=(0,1))[3],
+            'psnr_whole':err_mtx.mean(axis=(0,1))[4],
+            'mssim_whole': err_mtx.mean(axis=(0,1))[5],
+            },
+       'std':
+           {'rmse_noedge': err_mtx.std(axis=(0,1))[0],
+            'psnr_noedge': err_mtx.std(axis=(0,1))[1],
+            'mssim_noedge': err_mtx.std(axis=(0,1))[2],
+            'rmse_whole': err_mtx.std(axis=(0,1))[3],
+            'psnr_whole': err_mtx.std(axis=(0,1))[4],
+            'mssim_whole': err_mtx.std(axis=(0,1))[5],
+            }
+       }
+
+    # save:
+    experiment_file = os.path.join(analysis_dir, experiment_name + '.pkl')
+    with open(experiment_file, 'wb') as fp:
+        pkl.dump(err_compare, fp, protocol=pkl.HIGHEST_PROTOCOL)
+    print('Errors details saved as %s' % (experiment_file,))
 
 
-for dropout_rate in [0.1, 0.2, 0.3, 0.5]:
-    # Train:
-    opt['dropout_rate'] = dropout_rate
-    # Reconstruct (optional):
-    subjects_list = ['904044', '165840', '889579', '713239',
-                     '899885', '117324', '214423', '857263']
-    rmse_average = 0
-    for subject in subjects_list:
-        opt['subject'] = subject
-        rmse, _ = reconstruct_mcdropout.sr_reconstruct_mcdropout(opt)
-        rmse_average += rmse
 
-    print('\n Average RMSE on Diverse dataset is %.15f.'
-          % (rmse_average / len(subjects_list),))
+
--- experiments_miccai2017/run_analysis.py
+++ experiments_miccai2017/run_analysis.py
@@ -75,5 +75,3 @@ elif choose==2:
 
 
 
-
-
