c284ba28711c9e4ceab9d40995fd03b447051ad4
Ryutaro Tanno <Ryutaro.Tanno.15@ucl.ac.uk>
2017-04-25 18:34:45 +0100

    minor

--- largesc/run_me.py
+++ largesc/run_me.py
@@ -16,7 +16,7 @@ opt['n_h2'] = 2*opt['n_h1']
 opt['n_h3'] = 10
 
 # Training
-opt['overwrite'] = True  # restart the training completely.
+opt['overwrite'] = False  # restart the training completely.
 opt['continue'] = False  # set True if you want to continue training from the previous experiment
 if opt['continue']: opt['overwrite'] = False
 
