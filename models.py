ff01bd9855756e04a81adc27de32534d185b00de
Ryutaro Tanno <Ryutaro.Tanno.15@ucl.ac.uk>
2017-05-24 23:34:41 +0100

    made baseline network more generalisable

--- models.py
+++ models.py
@@ -183,6 +183,12 @@ def inference(method, x, y, keep_prob, opt, trade_off=None):
             h1_2 = conv3d(tf.nn.relu(h1_1), [3,3,3,n_h1,n_h2], [n_h2], 'conv_2')
             h1_2 = conv3d(tf.nn.relu(h1_2), [3,3,3,n_h2,n_h2], [n_h2], 'conv_3')
             h1_2 = conv3d(tf.nn.relu(h1_2), [3,3,3,n_h2,n_h2], [n_h2], 'conv_4')
+        elif opt['receptive_field_radius'] > 5:
+            h1_2 = conv3d(tf.nn.relu(h1_1), [3, 3, 3, n_h1, n_h2], [n_h2],'conv_2')
+            lyr=3
+            while lyr<(opt['receptive_field_radius']):
+                h1_2 = conv3d(tf.nn.relu(h1_1), [3, 3, 3, n_h2, n_h2], [n_h2], 'conv_'+str(lyr+1))
+                lyr+=1
 
         y_pred = conv3d(tf.nn.relu(h1_2),
                         [3,3,3,n_h2,no_channels*(upsampling_rate**3)],
