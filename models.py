6c17c19fa7e3597ac1ad5bc7ce3ebd28ad75e304
Ryutaro Tanno <Ryutaro.Tanno.15@ucl.ac.uk>
2017-05-26 10:23:59 +0100

    spotted a bug in model.py

--- models.py
+++ models.py
@@ -187,7 +187,7 @@ def inference(method, x, y, keep_prob, opt, trade_off=None):
             h1_2 = conv3d(tf.nn.relu(h1_1), [3, 3, 3, n_h1, n_h2], [n_h2],'conv_2')
             lyr=3
             while lyr<(opt['receptive_field_radius']):
-                h1_2 = conv3d(tf.nn.relu(h1_1), [3, 3, 3, n_h2, n_h2], [n_h2], 'conv_'+str(lyr+1))
+                h1_2 = conv3d(tf.nn.relu(h1_2), [3,3,3,n_h2,n_h2], [n_h2], 'conv_'+str(lyr+1))
                 lyr+=1
 
         y_pred = conv3d(tf.nn.relu(h1_2),
