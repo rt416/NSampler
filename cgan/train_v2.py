9accbf56415b5571184ea3e528cd128b06479d11
Ryutaro Tanno <Ryutaro.Tanno.15@ucl.ac.uk>
2017-06-13 09:53:06 +0100

    added bn param to the net name

--- cgan/train_v2.py
+++ cgan/train_v2.py
@@ -43,8 +43,9 @@ def name_network(opt):
     nn_var = (opt['upsampling_rate'],
               2*opt['input_radius']+1,
               2*opt['receptive_field_radius']+1,
-             (2*opt['output_radius']+1)*opt['upsampling_rate'])
-    nn_str = 'us=%i_in=%i_rec=%i_out=%i_'
+             (2*opt['output_radius']+1)*opt['upsampling_rate'],
+              opt['is_BN'])
+    nn_str = 'us=%i_in=%i_rec=%i_out=%i_bn=%i_'
 
     nn_var += (opt['no_subjects'],
                opt['no_patches'],
