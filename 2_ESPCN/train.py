4c98271accc56669e3d5ff0c07327b21c7be9192
Daniel Worrall <d.worrall@cs.ucl.ac.uk>
2017-06-18 22:58:06 +0100

    move network and checkpoint naming to utils

--- 2_ESPCN/train.py
+++ 2_ESPCN/train.py
@@ -1,27 +1,21 @@
 """Training file (Ryu) """
 
 import os
-import sys
-import timeit
 import glob
 import shutil
-import cPickle as pkl
+import sys
+import timeit
+sys.path.append("../common")
 
+import cPickle as pkl
 import numpy as np
 import tensorflow as tf
 
+import models
+
 from data_generator import prepare_data
-import models as models
 from ops import get_tensor_shape
-
-
-def define_checkpoint(opt):
-    nn_file = name_network(opt)
-    checkpoint_dir = os.path.join(opt['save_dir'], nn_file)
-    if not os.path.exists(checkpoint_dir):
-        print(checkpoint_dir)
-        os.makedirs(checkpoint_dir)
-    return checkpoint_dir
+from utils import *
 
 
 def define_logdir(opt):
@@ -32,41 +26,6 @@ def define_logdir(opt):
     return checkpoint_dir
 
 
-def name_network(opt):
-    """given inputs, return the model name."""
-    optim = opt['optimizer']
-
-    nn_header = opt['method'] if opt['dropout_rate']==0 \
-    else opt['method'] + str(opt['dropout_rate'])
-
-    opt['receptive_field_radius']=(2*opt['input_radius']-2*opt['output_radius'] + 1)//2
-
-    nn_var = (opt['upsampling_rate'],
-              opt['no_layers'],
-              2*opt['input_radius']+1,
-              2*opt['receptive_field_radius']+1,
-             (2*opt['output_radius']+1)*opt['upsampling_rate'],
-              opt['is_BN'])
-    nn_str = 'us=%i_lyr=%i_in=%i_rec=%i_out=%i_bn=%i_'
-
-    nn_var += (opt['no_subjects'],
-               opt['no_patches'],
-               opt['pad_size'],
-               opt['is_clip'],
-               opt['transform_opt'],
-               opt['patch_sampling_opt'],
-               opt['patchlib_idx'])
-    nn_str += 'ts=%d_pl=%d_pad=%d_clip=%i_nrm=%s_smpl=%s_%03i'
-    nn_body = nn_str % nn_var
-
-    if opt['is_map']:
-        nn_header = 'MAP_' + nn_header
-
-    if opt['valid']:
-        # Validate on the cost:
-        nn_header += '_validcost'
-
-    return nn_header + '_' + nn_body
 
 
 def name_patchlib(opt):
--- common/utils.py
+++ common/utils.py
@@ -6,3 +6,69 @@ import time
 
 import numpy as np
 import tensorflow as tf
+
+
+def define_checkpoint(opt):
+    """Create checkpoint directory.
+    
+    Generate checkpoint name and if directory does not exist, then instantiate
+    one.
+
+    Args:
+       opt: dict of options, must include save_dir field
+
+    Returns:
+        name of checkpoint directory
+    """
+    nn_file = name_network(opt)
+    checkpoint_dir = os.path.join(opt['save_dir'], nn_file)
+    if not os.path.exists(checkpoint_dir):
+        print(checkpoint_dir)
+        os.makedirs(checkpoint_dir)
+    return checkpoint_dir
+
+
+def name_network(opt):
+    """Return the model name.
+
+    Return a model name containing...
+
+    Args:
+        opt: dict of options
+
+    Returns:
+        model name string
+    """
+    optim = opt['optimizer']
+
+    nn_header = opt['method'] if opt['dropout_rate']==0 \
+    else opt['method'] + str(opt['dropout_rate'])
+
+    opt['receptive_field_radius']=(2*opt['input_radius']-2*opt['output_radius'] + 1)//2
+
+    nn_var = (opt['upsampling_rate'],
+              opt['no_layers'],
+              2*opt['input_radius']+1,
+              2*opt['receptive_field_radius']+1,
+             (2*opt['output_radius']+1)*opt['upsampling_rate'],
+              opt['is_BN'])
+    nn_str = 'us=%i_lyr=%i_in=%i_rec=%i_out=%i_bn=%i_'
+
+    nn_var += (opt['no_subjects'],
+               opt['no_patches'],
+               opt['pad_size'],
+               opt['is_clip'],
+               opt['transform_opt'],
+               opt['patch_sampling_opt'],
+               opt['patchlib_idx'])
+    nn_str += 'ts=%d_pl=%d_pad=%d_clip=%i_nrm=%s_smpl=%s_%03i'
+    nn_body = nn_str % nn_var
+
+    if opt['is_map']:
+        nn_header = 'MAP_' + nn_header
+
+    if opt['valid']:
+        # Validate on the cost:
+        nn_header += '_validcost'
+
+    return nn_header + '_' + nn_body
