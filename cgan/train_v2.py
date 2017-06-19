7fad8c9b32f53db653f146cea684c265b46cf96b
Ryutaro Tanno <Ryutaro.Tanno.15@ucl.ac.uk>
2017-06-19 14:25:22 +0100

    included number of filters in network names

--- cgan/train_v2.py
+++ cgan/train_v2.py
@@ -41,11 +41,12 @@ def name_network(opt):
 
     nn_var = (opt['upsampling_rate'],
               opt['no_layers'],
+              opt['no_filters'],
               2*opt['input_radius']+1,
               2*opt['receptive_field_radius']+1,
              (2*opt['output_radius']+1)*opt['upsampling_rate'],
               opt['is_BN'])
-    nn_str = 'us=%i_lyr=%i_in=%i_rec=%i_out=%i_bn=%i_'
+    nn_str = 'us=%i_lyr=%i_nf=%i_in=%i_rec=%i_out=%i_bn=%i_'
 
     nn_var += (opt['no_subjects'],
                opt['no_patches'],
