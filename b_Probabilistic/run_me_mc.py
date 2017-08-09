15da739076a049cb50c09d94e7a12782f7a5510b
Ryutaro Tanno <Ryutaro.Tanno.15@ucl.ac.uk>
2017-08-09 09:45:20 +0100

    moved stats.py to ./common. Also did some cleaning

--- b_Probabilistic/run_me_mc.py
+++ b_Probabilistic/run_me_mc.py
@@ -2,36 +2,32 @@
 
 import argparse
 import os
-import reconstruct
-import stats
 
 from common.data_utils import fetch_subjects
-from train import name_network, train_cnn
+import common.stats as stats
+import train
+import reconstruct
 
-# Settings
+
+# -------------------------- Set up configurations ----------------------------
+# Basic settings
 parser = argparse.ArgumentParser(description='dliqt-tensorflow-implementation')
 parser.add_argument('-e', '--experiment', dest='experiment', type=str, default='experiment_1', help='name of the experiment')
-parser.add_argument('--valid', action='store_true', help='pick the best model based on the loss, not the MSE?')
+parser.add_argument('--gpu', type=str, default="0", help='which GPU to use')
 parser.add_argument('--overwrite', action='store_true', help='restart the training completelu')
 parser.add_argument('--continue', action='store_true', help='continue training from previous epoch')
 parser.add_argument('--is_reset', action='store_true', help='reset the patch library?')
 parser.add_argument('--is_samples', action='store_true', help='want to monitor intermediate samples?')
-parser.add_argument('--gpu', type=str, default="0", help='which GPU to use')
 parser.add_argument('--not_save', action='store_true', help='evoke not to save reconstructed output.')
 parser.add_argument('--disp', action='store_true', help='save the displayed outputs?')
 parser.add_argument('-pp', '--postprocess', dest='postprocess', action='store_true', help='post-process the estimated highres output?')
 
-parser.add_argument('--optimizer', type=str, default='adam', help='optimization method')
-parser.add_argument('-lr', '--learning_rate', dest='learning_rate', type=float, default='1e-3', help='learning rate')
-parser.add_argument('--no_epochs', type=int, default=200, help='number of epochs to train for')
-parser.add_argument('--batch_size', type=int, default=12, help='batch size')
-parser.add_argument('--validation_fraction', type=float, default=0.5, help='fraction of validation data')
-parser.add_argument('--patch_sampling_opt', type=str, default='default', help='sampling scheme for patche extraction')
-parser.add_argument('--transform_opt', type=str, default='standard', help='normalisation transform')
-parser.add_argument('--pad_size', type=int, default=-1, help='size of padding. Set -1 to apply maximal padding.')
-parser.add_argument('--is_clip', action='store_true', help='want to clip the images for preprocessing?')
-parser.add_argument('--is_shuffle', action='store_true', help='want to reverse shuffle the HR output into LR space?')
-parser.add_argument('--is_dt_all', action='store_true', help='use the dt_all files')
+# Directories:
+parser.add_argument('--base_dir', type=str, default='/SAN/vision/hcp/Ryu/miccai2017', help='base directory')
+parser.add_argument('--gt_dir', type=str, default='/SAN/vision/hcp/DCA_HCP.2013.3_Proc', help='ground truth directory')
+parser.add_argument('--subpath', type=str, default='T1w/Diffusion', help='subdirectory in gt_dir')
+parser.add_argument('--mask_dir', type=str, default='/SAN/vision/hcp/Ryu/miccai2017/hcp_masks', help='directory of segmentation masks')
+parser.add_argument('--mask_subpath', type=str, default='', help='subdirectory in mask_dir')
 
 # Model:
 parser.add_argument('-m', '--method', dest='method', type=str, default='espcn', help='network type')
@@ -48,6 +44,14 @@ parser.add_argument('--hybrid_on', action='store_true', help='want to perform hy
 parser.add_argument('--is_BN', action='store_true', help='want to use batch normalisation?')
 parser.add_argument('-dr', '--dropout_rate', dest='dropout_rate', type=float, default='0.0', help='drop-out rate')
 
+parser.add_argument('--optimizer', type=str, default='adam', help='optimization method')
+parser.add_argument('-lr', '--learning_rate', dest='learning_rate', type=float, default='1e-3', help='learning rate')
+parser.add_argument('--no_epochs', type=int, default=200, help='number of epochs to train for')
+parser.add_argument('--batch_size', type=int, default=12, help='batch size')
+parser.add_argument('--validation_fraction', type=float, default=0.5, help='fraction of validation data')
+parser.add_argument('--valid', action='store_true', help='pick the best model based on the loss, not the MSE?')
+
+
 # Data/task
 parser.add_argument('--is_map', action='store_true', help='MAP-SR?')
 parser.add_argument('-pl', '--no_patches', dest='no_patches', type=int, default=2250, help='number of patches sampled from each train subject')
@@ -56,13 +60,13 @@ parser.add_argument('-bgval', '--background_value', dest="background_value", typ
 parser.add_argument('--no_channels', type=int, default=6, help='number of channels')
 parser.add_argument('-us', '--upsampling_rate', dest="upsampling_rate", type=int, default=2, help='upsampling rate')
 parser.add_argument('-ir', '--input_radius', dest="input_radius", type=int, default=5, help='input radius')
+parser.add_argument('--patch_sampling_opt', type=str, default='default', help='sampling scheme for patche extraction')
+parser.add_argument('--transform_opt', type=str, default='standard', help='normalisation transform')
+parser.add_argument('--pad_size', type=int, default=-1, help='size of padding applied before patch extraction. Set -1 to apply maximal padding.')
+parser.add_argument('--is_clip', action='store_true', help='want to clip the images (0.1% - 99.9% percentile) for preprocessing?')
+parser.add_argument('--is_shuffle', action='store_true', help='want to reverse shuffle the HR output into LR space?')
+parser.add_argument('--is_dt_all', action='store_true', help='use the dt_all files')
 
-# Directories:
-parser.add_argument('--base_dir', type=str, default='/SAN/vision/hcp/Ryu/miccai2017', help='base directory')
-parser.add_argument('--gt_dir', type=str, default='/SAN/vision/hcp/DCA_HCP.2013.3_Proc', help='ground truth directory')
-parser.add_argument('--subpath', type=str, default='T1w/Diffusion', help='subdirectory in gt_dir')
-parser.add_argument('--mask_dir', type=str, default='/SAN/vision/hcp/Ryu/miccai2017/hcp_masks', help='directory of segmentation masks')
-parser.add_argument('--mask_subpath', type=str, default='', help='subdirectory in mask_dir')
 
 arg = parser.parse_args()
 opt = vars(arg)
@@ -126,7 +130,7 @@ for key, val in opt.iteritems():
     print("{}: {}".format(key, val))
 
 # TRAIN
-train_cnn(opt)
+train.train_cnn(opt)
 
 
 # RECONSTRUCT:
@@ -137,17 +141,3 @@ for subject in subjects_list:
 
 # STATS:
 stats.compute_stats(opt, subjects_list)
-
-
-# subjects_list = fetch_subjects(no_subjects=8, shuffle=False, test=True)
-# rmse_average = 0
-# rmse_whole_average = 0
-# for subject in subjects_list:
-#     opt['subject'] = subject
-#     rmse, rmse_whole = reconstruct.sr_reconstruct(opt)
-#     rmse_average += rmse
-#     rmse_whole_average += rmse_whole
-# print('\n Average RMSE (interior) on Diverse dataset is %.15f.'
-#       % (rmse_average / len(subjects_list),))
-# print('\n Average RMSE (whole) on Diverse dataset is %.15f.'
-#       % (rmse_whole_average / len(subjects_list),))
